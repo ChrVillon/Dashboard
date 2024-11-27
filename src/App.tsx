efd997103bcdb46860dc6b36ec1c2eebe026820d
chrVillon <chrovill@espol.edu.ec>
2024-11-27 10:24:38 -0500

    avance quia 13 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import { useState } from 'react'
+import { useEffect } from 'react'
 import Grid from '@mui/material/Grid2'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
@@ -8,34 +8,81 @@ import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
 
+interface Indicator {
+  title?: String;
+  subtitle?: String;
+  value?: String;
+}
+
 function App() {
   //const [count, setCount] = useState(0)
+
+  {/* Hook: useEffect */ }
+  useEffect(() => {
+    let request = async () => {
+      {/* Request */ }
+      let API_KEY = "OPENWEATHERMAP' API KEY"
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+      let savedTextXML = await response.text();
+
+      {/* XML Parser */ }
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+      {/* Arreglo para agregar los resultados */ }
+
+      let dataToIndicators: Indicator[] = new Array<Indicator>();
+
+      {/* 
+                 Análisis, extracción y almacenamiento del contenido del XML 
+                 en el arreglo de resultados
+             */}
+
+      let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "City", "value": name })
+
+      let location = xml.getElementsByTagName("location")[1]
+
+      let latitude = location.getAttribute("latitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+      let longitude = location.getAttribute("longitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+      let altitude = location.getAttribute("altitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+      console.log(dataToIndicators)
+    }
+
+    request();
+  }, [])
 
   return (
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 3, xl: 12 }}><IndicatorWeather title={'Indicador 1'} subtitle={'Unidad 1'} value={'1.23'} /></Grid>
-      <Grid size={{ xs: 3, xl: 12 }}><IndicatorWeather title={'Indicador 2'} subtitle={'Unidad 2'} value={'3.12'} /></Grid>
-      <Grid size={{ xs: 3, xl: 12 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} /></Grid>
-      <Grid size={{ xs: 3, xl: 12 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicador 1'} subtitle={'Unidad 1'} value={'1.23'} /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicador 2'} subtitle={'Unidad 2'} value={'3.12'} /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} /></Grid>
 
       {/* Tabla */}
-      <Grid size={{ xs: 8, xl: 5 }}>
+      <Grid size={{ xs: 12, xl: 8 }}>
         {/* Grid Anidado */}
         <Grid container spacing={2}>
-          <Grid size={{ xs: 12, xl: 3 }}>
+          <Grid size={{ xs: 12, xl: 9 }}>
             <ControlWeather />
           </Grid>
-          <Grid size={{ xs: 12, xl: 9 }}>
+          <Grid size={{ xs: 9, xl: 12 }}>
             <TableWeather />
           </Grid>
         </Grid>
       </Grid>
 
       {/* Gráfico */}
-      <Grid size={{ xs: 4, xl: 12 }}>
-      <LineChartWeather/>
+      <Grid size={{ xs: 12, xl: 4 }}>
+        <LineChartWeather />
       </Grid>
 
     </Grid>
