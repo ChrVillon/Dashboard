01e3c52bd661fcd1b4d331ad9de2d3f4bbe58671
ChrVillon <chrovill@espol.edu.ec>
2024-11-27 10:46:03 -0500

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Grid from '@mui/material/Grid2'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
@@ -16,10 +16,13 @@ interface Indicator {
 
 function App() {
   //const [count, setCount] = useState(0)
+  {/* Variable de estado y función de actualización */ }
+  let [indicators, setIndicators] = useState<Indicator[]>([])
 
   {/* Hook: useEffect */ }
   useEffect(() => {
     let request = async () => {
+
       {/* Request */ }
       let API_KEY = "OPENWEATHERMAP' API KEY"
       let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
@@ -52,7 +55,9 @@ function App() {
       let altitude = location.getAttribute("altitude") || ""
       dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
 
-      console.log(dataToIndicators)
+      //console.log(dataToIndicators)
+      {/* Variable de estado y función de actualización */ }
+      let [indicators, setIndicators] = useState<Indicator[]>([])
     }
 
     request();
@@ -62,30 +67,46 @@ function App() {
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicador 1'} subtitle={'Unidad 1'} value={'1.23'} /></Grid>
+      {/*<Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicador 1'} subtitle={'Unidad 1'} value={'1.23'} /></Grid>
       <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicador 2'} subtitle={'Unidad 2'} value={'3.12'} /></Grid>
       <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} /></Grid>
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} /></Grid>
+      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} /></Grid>*/}
 
-      {/* Tabla */}
-      <Grid size={{ xs: 12, xl: 8 }}>
-        {/* Grid Anidado */}
-        <Grid container spacing={2}>
-          <Grid size={{ xs: 12, xl: 9 }}>
-            <ControlWeather />
-          </Grid>
-          <Grid size={{ xs: 9, xl: 12 }}>
-            <TableWeather />
-          </Grid>
-        </Grid>
-      </Grid>
-
-      {/* Gráfico */}
-      <Grid size={{ xs: 12, xl: 4 }}>
-        <LineChartWeather />
-      </Grid>
+      {
+        indicators
+          .map(
+            (indicator, idx) => (
+              <Grid key={idx} size={{ xs: 12, xl: 3 }}>
+                <IndicatorWeather
+                  title={indicator["title"]}
+                  subtitle={indicator["subtitle"]}
+                  value={indicator["value"]} />
+              </Grid>
+            )
+          )
+      }
+      
 
     </Grid>
+
+      {/* Tabla */ }
+  <Grid size={{ xs: 12, xl: 8 }}>
+    {/* Grid Anidado */}
+    <Grid container spacing={2}>
+      <Grid size={{ xs: 12, xl: 9 }}>
+        <ControlWeather />
+      </Grid>
+      <Grid size={{ xs: 9, xl: 12 }}>
+        <TableWeather />
+      </Grid>
+    </Grid>
+  </Grid>
+
+  {/* Gráfico */ }
+  <Grid size={{ xs: 12, xl: 4 }}>
+    <LineChartWeather />
+  </Grid>
+
   )
 }
 
